3141e027ea04f7b6918990ea4b65823a6f23294f
chrisedebo <chris.nicel@15below.com>
2013-08-06 14:58:58 +0100

    Added test file maker

---
+++ RabbitMQ.LoadTest.TestFileMaker/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace RabbitMQ.LoadTest.TestFileMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get text to put into files.
+            System.IO.StreamReader textFile =
+               new System.IO.StreamReader("Text.txt");
+            string textString = textFile.ReadToEnd();
+
+            textFile.Close();
+
+            //Create test files directory if it does not exist.
+            string testfilesPath = "testfiles";
+
+            bool isExists = System.IO.Directory.Exists(testfilesPath);
+
+            if (!isExists)
+                System.IO.Directory.CreateDirectory(testfilesPath);
+            
+
+            // Read the lengths file and put textstring section into files.
+            int counter = 0;
+            string length;
+
+            System.IO.StreamReader lengthsFile =
+               new System.IO.StreamReader("MessageLengths.txt");
+            while ((length = lengthsFile.ReadLine()) != null)
+            {
+                System.IO.StreamWriter outFile = new System.IO.StreamWriter(".\\testfiles\\testfile_" + counter.ToString() + ".txt");
+                outFile.Write(textString.Substring(0,Convert.ToInt32(length)));
+                outFile.Close();
+                counter++;
+            }
+
+            lengthsFile.Close();
+            
+        }
+    }
+}
